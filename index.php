35acdd012ba91c45b2bfa0ab29d10e6ee8adb957
guiguivida183882 <guikerme.1707@gmail.com>
2023-01-16 12:00:50 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    
+    <title>GSA SITE LANDING PAG</title>
+</head>
+<body>
+    
+    <section>
+        <div class="circle"></div>
+        <?php include('header.html')?>
+            <div class="content">
+                <div class="text">
+                    <h2>SEU SITE ESTÁ<br><span>AQUI</span></h2>
+                    <p>Somos uma empresa totalmente qualifica no mercado da tecnologia, com capacidade no desenvolvimento do seu site.<br>"Cuide de outras coisas enquanto cuidamos do seu site" </p>
+                    <a href="#">FAÇA SEU ORÇAMENTO</a>
+                </div>
+            </div>
+            <ul class="icons">
+                <li><a href="#"><img src="img/facebook.png" alt=""></a></li>
+                <li><a href="#"><img src="img/twitter.png" alt=""></a></li>
+                <li><a href="#"><img src="img/instagram.png" alt=""></a></li>
+            </ul>
+            <div class="boxImg">
+                <img src="img/logotst.webp" alt="" class="img1">
+            </div>
+    </section>
+</body>
+</html>
